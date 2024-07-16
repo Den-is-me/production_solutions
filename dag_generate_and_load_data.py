273a242a7a7042f8b2bf7f6a37ffedb03d83cfc2
Denis Suchkov <107809488+Den-is-me@users.noreply.github.com>
2024-07-16 10:47:31 +0500

    Update dag_generate_and_load_data.py

--- dag_generate_and_load_data.py
+++ dag_generate_and_load_data.py
@@ -3,64 +3,131 @@ from airflow.contrib.operators.ssh_operator import SSHOperator
 from airflow.decorators import task
 from airflow.operators.postgres_operator import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
-from airflow.sensors.filesystem import FileSensor
-from airflow.operators.bash import BashOperator
 from airflow.operators.email import EmailOperator
 from airflow.providers.oracle.operators.oracle import OracleOperator
+from airflow.providers.sftp.sensors.sftp import SFTPSensor
+from airflow.utils.task_group import TaskGroup
 
 from datetime import timedelta, datetime
 
 
-default_args = {
-
-}
-
-
-@task.branch
+@task
 def get_mask(**context):
-
-    churn_dt = pg_hook_test.get_first(sql_query_churn_dt)[0]
-    mask = churn_dt.strftime('%Y%m%d')
-
-    profile_dt = pg_hook_prod.get_first(sql_query_profile_dt)[0]
-
-    print(mask, '<----------------- mask')
-
-    context['ti'].xcom_push(key='mask', value=mask)
-    context['ti'].xcom_push(key='partition', value=profile_dt.strftime('%Y%m%d'))
-    context['ti'].xcom_push(key='partition_exclusive', value=(profile_dt + timedelta(days=1)).strftime('%Y%m%d'))
-    context['ti'].xcom_push(key='drop_partition', value=(profile_dt - timedelta(days=30)).strftime('%Y%m%d'))
-
-    if context['dag'].timezone.convert(context['ti'].start_date).date() - churn_dt > timedelta(days=7):
-        return ['churn_alert', 'create_evol_churn', 'create_new_partition']
-    return ['create_evol_churn', 'create_new_partition']
+	pass
 
 @task
-def check_device_field(**context):
-    pg_hook = PostgresHook(postgres_conn_id='prod_greenplum_gpadmin')
-    sql_query = "select count(1) from cvm.evol_profile where device_type is not Null;"
-    check_result = pg_hook.get_first(sql_query)[0]
+def check_fields(**context):
+	pass
 
-    print(check_result, '<----------------- amount of devices')
 
-    if not check_result:
-        raise ValueError('There is no value in the profile device field')
-
+default_args = {
+    'start_date':  datetime(2024, 7, 15),
+    'retries': 0,
+    'retry_delay': timedelta(minutes=30),
+    'email_on_failure': True,
+    'email': ['DSuchkov@beeline.uz'],
+    'do_xcom_push': False,
+    'owner': 'Denis',
+}
 
 with DAG(
-        dag_id='evol_generate_profile_morning',
+        dag_id='generate_partprofile',
         default_args=default_args,
-        schedule_interval='@daily',
+        schedule_interval='0 6 * * *',
         catchup=False,
         max_active_runs=1,
         tags=['evolution'],
+        params={'load_date_%d.%m.%Y': None},
+        description='''
+         DAG creates and uploads to Evolution system:
+         1. morning part of CVM profile.
+         2. daily canceled subs.
+         3. daily product catalog.
+         4. save previous profile into profile_history table.
+         If you want to use a specific date for profile, please use manual trigger with param "load_date_%d.%m.%Y". 
+         ''',
     ) as dag:
 
-    check_profile_flag = FileSensor(
+
+    check_subs_flag = SFTPSensor(
 
     )
 
     get_mask = get_mask()
+
+    with TaskGroup('create_temp_tables') as create_temp_tables:
+
+        check_agg_snp_prepaid_flag = SFTPSensor(
+
+        )
+
+        create_cvm_profile_subs_tmp = OracleOperator(
+
+        )
+
+        create_cvm_profile_soc_pp_tmp = OracleOperator(
+
+        )
+
+        create_cvm_profile_age_tmp = OracleOperator(
+
+
+        )
+
+        create_gp_profile_imei_tmp = PostgresOperator(
+
+        )
+
+        create_profile_flag_table = PostgresOperator(
+
+        )
+
+        create_cvm_profile_services_tmp = OracleOperator(
+
+        )
+
+        create_evol_churn = PostgresOperator(
+
+        )
+
+        truncate_cvm_profile_recharges_tmp= OracleOperator(
+
+        )
+
+        truncate_cvm_profile_arpu_tmp= OracleOperator(
+
+        )
+
+        check_bar_cvm_flag = SFTPSensor(
+    
+        )
+
+        check_agg_snp_prepaid_flag >> create_cvm_profile_subs_tmp
+        create_ora_cvm_profile_imei_tmp >> create_gp_profile_imei_tmp
+
+
+    create_evol_profile_test = PostgresOperator(
+
+    )
+
+    create_csv_profile = PostgresOperator(
+
+
+    )
+
+    check_fields = check_fields()
+
+    profile_alert = EmailOperator(
+
+    )
+
+    remove_bar_cvm_flag = SSHOperator(
+
+    )
+
+    #-----------------------------------------
+    # SAVE PROFILE IN HISTORY
+    # -----------------------------------------
 
     create_new_partition = PostgresOperator(
 
@@ -74,24 +141,11 @@ with DAG(
 
     )
 
-    create_profile_flag_table = PostgresOperator(
-    )
-    churn_alert = EmailOperator(
+    #-----------------------------------------
 
-    )
-
-    create_evol_churn = PostgresOperator(
-
-    )
-
-    create_evol_profile = PostgresOperator(
-
-    )
-
-    create_csv_profile = PostgresOperator(
-
-    )
-
+    #-----------------------------------------
+    # CANCELED SUBS PREVIOUS DAY
+    # -----------------------------------------
     create_csv_canceled = PostgresOperator(
 
     )
@@ -100,18 +154,9 @@ with DAG(
 
     )
 
-    upload_profile_to_evol = SSHOperator(
-
-    )
-
-    remove_profile_flag = BashOperator(
-
-
-    )
-
-    send_profile_success_upload = EmailOperator(
-
-    )
+    #-----------------------------------------
+    # PRODUCT CATALOG
+    # -----------------------------------------
 
     run_proc_product_catalogue = OracleOperator(
 
@@ -124,18 +169,12 @@ with DAG(
     upload_catalog_to_evol = SSHOperator(
 
     )
-
-    check_device_field = check_device_field()
-
+    # -----------------------------------------
 
 
-
-    check_profile_flag >> get_mask >> create_evol_churn >> create_evol_profile >> create_csv_profile >> upload_profile_to_evol >> remove_profile_flag
-    check_profile_flag >> create_csv_canceled >> upload_canceled_to_evol >> remove_profile_flag
-    check_profile_flag >> run_proc_product_catalogue >> create_csv_catalog >> upload_catalog_to_evol >> remove_profile_flag
-    check_profile_flag >> create_profile_flag_table >> create_evol_profile
-    upload_profile_to_evol >> send_profile_success_upload
-    upload_profile_to_evol >> check_device_field
-    get_mask >> churn_alert
-    get_mask >> create_new_partition >> save_previous_profile_in_history >> drop_old_partitions
-    save_previous_profile_in_history >> create_evol_profile
+(check_subs_flag >> get_mask >> create_temp_tables >> create_evol_profile_test >>
+ create_csv_profile >> upload_profile_to_evol >> check_fields >> profile_alert)
+upload_profile_to_evol >> remove_bar_cvm_flag
+check_subs_flag >> run_proc_product_catalogue >> create_csv_catalog >> upload_catalog_to_evol
+check_subs_flag >> create_csv_canceled >> upload_canceled_to_evol
+get_mask >> create_new_partition >> save_previous_profile_in_history >> drop_old_partitions >> create_evol_profile_test
